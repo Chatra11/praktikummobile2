9f14879d32efcdcf1a266e9f0da0f0c25e67c636
Al Chatra Syarifudin <101439279+Chatra11@users.noreply.github.com>
2022-03-20 14:55:13 +0800

    Prak104.kt

--- modul1/PRAK104.kt
+++ modul1/PRAK104.kt
@@ -1,16 +1,23 @@
 class hewan (nama : String , kaki :Int, hidup : String){
-    var Nama : String = nama
-    var Kaki : Int  = kaki
-    var Habitat : String = hidup
+    private var Nama : String = nama
+    private var Kaki : Int  = kaki
+    private var Habitat : String = hidup
 
     fun tampil(){
         println("Nama Hewan : $Nama")
         println("Kaki Hewan : $Kaki")
-        println("Habitat Hewan : $Habitat")
+        println("Habitat Hewan : $Habitat\n")
     }
 }
 fun main(){
-    val Hewan1 = hewan("Gajah",4,"Darat")
+    print("Nama Hewan : ")
+    val Nama = readLine()!!
+    print("Kaki hewan : ")
+    val Kaki = readLine()!!.toInt()
+    print("Habitat Hewan : ")
+    val Tempat = readLine()!!
+
+    val Hewan1 = hewan(Nama,Kaki,Tempat)
     val Hewan2 = hewan("Buaya", 4 , "Darat dan Air")
     val Hewan3 = hewan("Ikan axoloti " ,4, "air")
 
